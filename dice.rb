ae5f8b5320fe0ac0c78b9477337ba0b6a279bf34
jake77895 <jspitzer@chicagobooth.edu>
2024-10-22 17:30:33 +0000

    Commit

--- dice.rb
+++ dice.rb
@@ -1,5 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 
 get "/" do
   @message = "Welcome to the Dice Roll App!"
@@ -21,13 +29,16 @@ get("/dice/2/6") do
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
 
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  # Render the outcome with a reroll button
+  <<-HTML
+  <h1>2d6</h1>
+  <p>#{outcome}</p>
+  <a href="/dice/2/6" style="display: inline-block; margin-top: 20px; text-decoration: none; padding: 10px 20px; background-color: #4CAF50; color: white; border-radius: 5px;">Reroll</a>
+
+  <a href="/" style="display: inline-block; margin-top: 20px; text-decoration: none; padding: 10px 20px; background-color: #4CAF50; color: white; border-radius: 5px;">Home</a>
+  HTML
 
 end
-
-
-
 
 get("/dice/2/10") do
   first_die = rand(1..10)
